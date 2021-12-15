9efbe394fdc084c06ed5f8b74d6f62e6916e089b
Robert Starr <rstarr@indeed.com>
2021-12-15 10:56:00 -0800

    Added mailto link.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ghostwritingStationeryLogo from './Images/ghostwritingStationeryLogo.png'
 import './App.css';
 import {useEffect} from "react";
+import Obfuscate from 'react-obfuscate';
 
 function App() {
     useEffect(() => {
@@ -10,6 +11,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={ghostwritingStationeryLogo} className="App-logo" alt="logo" />
+          <div className={'App-link'}>
+          Contact: <Obfuscate email={"robert@robertkstarr.com"} />
+          </div>
       </header>
     </div>
   );
