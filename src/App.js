b9d2d440c7e1e21a9a653cdaa879cc8de57febca
Robert Starr <rstarr@indeed.com>
2021-12-15 12:48:16 -0800

    Added email and changed font.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import ghostwritingStationeryLogo from './Images/ghostwritingStationeryLogo.png'
-import './App.css';
-import {useEffect} from "react";
-import Obfuscate from 'react-obfuscate';
+import starrGhostwritingLogo from "./Images/starrGhostwritingLogo.png";
+import "./App.css";
+import { useEffect } from "react";
+import Obfuscate from "react-obfuscate";
 
 function App() {
-    useEffect(() => {
-        document.title = "Starr Ghostwriting Services"
-    }, []);
+  useEffect(() => {
+    document.title = "Starr Ghostwriting Services";
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ghostwritingStationeryLogo} className="App-logo" alt="logo" />
-          <div className={'App-link'}>
-          Contact: <Obfuscate email={"robert@robertkstarr.com"} />
+        <img src={starrGhostwritingLogo} className="App-logo" alt="logo" />
+        <div className={"App-link"}>
+          CONTACT:
+          <div>
+            <Obfuscate email={"robert@robertkstarr.com"} />
           </div>
+        </div>
       </header>
     </div>
   );
