23587417fc089f4a919f790acbfcda94fd423c7d
Robert Starr <rstarr@indeed.com>
2021-12-21 15:23:47 -0800

    Added mobile versions along with autodetect.

--- src/SiteHeader/HeaderItem/HeaderItem.js
+++ src/SiteHeader/HeaderItem/HeaderItem.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./HeaderItem.css";
+import { isMobile } from "react-device-detect";
 
 const HeaderItem = ({ headerText }) => {
-  return <div className="HeaderItem">{headerText.toUpperCase()}</div>;
+  return (
+    <div className={`HeaderItem ${isMobile && `isMobile`}`}>
+      {" "}
+      {headerText.toUpperCase()}
+    </div>
+  );
 };
 
 export default HeaderItem;
--- src/SiteHeader/SiteHeader.js
+++ src/SiteHeader/SiteHeader.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./SiteHeader.css";
 import HeaderItem from "./HeaderItem/HeaderItem";
+import { isMobile } from "react-device-detect";
 
 const SiteHeader = () => {
+  const deviceType = isMobile ? "Mobile" : "Desktop";
   return (
-    <header className="SiteHeader">
+    <header className={`SiteHeader ${deviceType}`}>
       <HeaderItem headerText={"Home"} />
       <HeaderItem headerText={"Samples"} />
       <HeaderItem headerText={"Works"} />
