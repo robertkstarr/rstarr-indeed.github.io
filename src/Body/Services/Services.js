e7b8d29021b866cfcb894e54d6c329503b868140
Robert Starr <robert.k.starr@gmail.com>
2024-06-10 10:14:30 -0700

    Switched Services to use TextBox

--- src/Body/Services/Services.js
+++ src/Body/Services/Services.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Services.css";
+import TextBox from "../../Tools/TextBox/TextBox";
 
 export const SERVICES_LABEL = "Services";
 
 const Services = () => {
   return (
     <div className={"Services"}>
-      <div>A list of services along with the price per word.</div>
-      <div>
-        Include genres along with descriptions and well-known examples of each
-        of the genres.
-      </div>
+      <TextBox>
+        <div>A list of services along with the price per word.</div>
+        <div>
+          Include genres along with descriptions and well-known examples of each
+          of the genres.
+        </div>
+      </TextBox>
     </div>
   );
 };
